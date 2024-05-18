135b2f195f78901dcc222fbcf30a148875bdd481
Rafael Yuki <rafael.tobaru@edu.unifil.br>
2024-05-18 10:08:43 -0300

    removendo alguns comentários

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-// Definir o caminho correto para o autoload.php
 require_once(__DIR__ . '/application/autoload.php');
 
 // Definir rotas
@@ -12,7 +11,6 @@ $routes = [
     'fornecedor/editar' => 'Application/views/fornecedor/fornecedor_edit.php',
     'fornecedor/visualizar' => 'Application/views/fornecedor/fornecedor_view.php',
     'fornecedor/atualizar' => 'Application/controllers/fornecedor_controller.php',
-    // Adicione mais rotas conforme necessário
 ];
 
 // Obter o caminho da URL
@@ -20,11 +18,9 @@ $path = isset($_GET['url']) ? $_GET['url'] : '';
 
 // Verificar se a rota existe
 if (array_key_exists($path, $routes)) {
-    // Incluir o arquivo correspondente à rota
     require_once(__DIR__ . '/' . $routes[$path]);
 } else {
     // Rota não encontrada, redirecionar para página de erro ou página inicial
-    // Exemplo de redirecionamento para a página inicial:
     header('Location: /');
     exit;
 }
